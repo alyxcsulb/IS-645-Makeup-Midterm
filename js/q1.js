840126b3003ae7138eb93bd87183db66920779de
Alyx Faber <alyx.faber@student.csulb.edu>
2021-05-15 15:46:52 -0700

    first commit

---
+++ js/q1.js
@@ -0,0 +1,8 @@
+// include:
+// ask user for input 
+// input must be numeric
+// if input is invalid display red font error message
+// perform calculation with a loop
+// display a title that includes the user input
+// output the results in an HTML table
+// set the row background color based on some criteria
---
+++ js/q2.js
@@ -0,0 +1,34 @@
+// provide the sutdent with an array of objects
+// create a summary array
+// use map, reduce and/or filter
+// array must perform at least 2 calculations
+// using the summary array, creat a new filtered array based on some criteria
+// display output using a specific format
+// dispaly the filtered array using console.log
+
+// Given: An Array of objects.
+// Each object contains:
+//  - blah blah
+//  - …
+
+/*   const myArray = [
+        {
+        Object 1 Properties
+        },
+        {
+        Object 2 Properties
+        },
+        …
+    ];
+    
+    // PART I – Write requirements 
+    
+    << YOUR CODE HERE >>
+    
+    // PART II – Write requirements 
+    
+    << YOUR CODE HERE >> */
+  
+  
+  // Etc….
+  
